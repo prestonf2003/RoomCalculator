05d6ac5412dbc1898c5c2e5e6d47b10a701a046b
prestonf2003 <prestonfernando@gmail.com>
2022-04-11 14:48:17 -0400

    Add project files.

---
+++ RoomCalculator/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Please enter the length of your Classroom: ");
+        string length = Console.ReadLine();
+        Console.WriteLine("Please enter the width of your Classroom: ");
+        string width = Console.ReadLine(); 
+        double l = double.Parse(length);
+        double w = double.Parse(width);
+        double area = l * w;
+        double perimeter = l * 2 + w * 2;
+
+        Console.WriteLine($"the area of your classroom is: {area}");
+        Console.WriteLine($"the perimeter of your classroom is: {perimeter}");
+
+    }
+}
